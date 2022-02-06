7cdb089299c8d72615c48843de0eb35e4a0eb41f
Muhammad Irfan <mrfansi@outlook.com>
2022-02-06 09:01:03 +0700

    Update

--- src/xendit.js
+++ src/xendit.js
@@ -1,39 +1,40 @@
 'use strict';
 
-var Card = require('./card');
+const Card = require('./card');
 
-var STAGING_XENDIT_BASE_URL = 'https://api-staging.xendit.co';
-var PRODUCTION_XENDIT_BASE_URL = 'https://api.xendit.co';
+const STAGING_XENDIT_BASE_URL = 'https://api-staging.xendit.co';
+const PRODUCTION_XENDIT_BASE_URL = 'https://api.xendit.co';
 
-function Xendit() {}
+function Xendit() {
+}
 
 Xendit.settings = {
-    url: PRODUCTION_XENDIT_BASE_URL
+  url: PRODUCTION_XENDIT_BASE_URL
 };
 
 Xendit.card = new Card(Xendit);
 
 Xendit.setPublishableKey = function setPublishableKey(publishableKey) {
-    Xendit.settings.publishable_key = publishableKey;
+  Xendit.settings.publishable_key = publishableKey;
 };
 
 Xendit._useStagingURL = function (toggle) {
-    Xendit.settings.url = toggle === false ? PRODUCTION_XENDIT_BASE_URL : STAGING_XENDIT_BASE_URL;
+  Xendit.settings.url = toggle === false ? PRODUCTION_XENDIT_BASE_URL : STAGING_XENDIT_BASE_URL;
 };
 
 Xendit._getXenditURL = function () {
-    return Xendit.settings.url;
+  return Xendit.settings.url;
 };
 
 Xendit._getPublishableKey = function () {
-    return Xendit.settings.publishable_key;
+  return Xendit.settings.publishable_key;
 };
 
 Xendit._getEnvironment = function () {
-    var normalizedKey = this._getPublishableKey().toUpperCase();
-    var isKeyProduction = normalizedKey.indexOf('PRODUCTION') > -1;
+  const normalizedKey = this._getPublishableKey().toUpperCase();
+  const isKeyProduction = normalizedKey.indexOf('PRODUCTION') > -1;
 
-    return isKeyProduction ? 'PRODUCTION' : 'DEVELOPMENT';
+  return isKeyProduction ? 'PRODUCTION' : 'DEVELOPMENT';
 };
 
 // global.Xendit = Xendit;
